8b03bac01f95cd48b4f43e3bc394a9aa533102df
jiye-7 <jy522hi@gmail.com>
2023-03-04 01:40:14 +0900

    feat: tailwindcss 설정

---
+++ src/TailwindComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TailwindComponent() {
+  return (
+    <div>
+      <h1 className='text-7xl'>안녕하세요.</h1>
+      <button className='bg-blue-400 rounded-xl px-4'>Button!</button>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // project에서 쓰는 파일 경로 설정 (공백 주의)
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
